77373baa0e7f805a2469e243e40c63feb877dd94
dbraquart <107846716+dbraquart@users.noreply.github.com>
2024-12-19 15:05:33 +0100

    Store and manage network visualization parameters (#15)
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

---
+++ src/main/java/org/gridsuite/spreadsheetconfig/server/dto/NetworkAreaDiagramParamInfos.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.spreadsheetconfig.server.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+/**
+ * @author David BRAQUART <david.braquart at rte-france.com>
+ */
+@Schema(name = "NetworkAreaDiagramParamDto", description = "Network area diagram parameters")
+public record NetworkAreaDiagramParamInfos(
+
+    @Schema(description = "Initialize with geographical data")
+    Boolean initNadWithGeoData
+) { }
---
+++ src/main/java/org/gridsuite/spreadsheetconfig/server/entities/NetworkVisualizationParamEntity.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.spreadsheetconfig.server.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.UUID;
+
+/**
+ * @author David BRAQUART <david.braquart at rte-france.com>
+ */
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "network_visualization_params")
+public class NetworkVisualizationParamEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "line_full_path")
+    private Boolean lineFullPath = true;
+
+    @Column(name = "line_parallel_path")
+    private Boolean lineParallelPath = true;
+
+    @Column(name = "line_flow_mode")
+    private String lineFlowMode = "feeders";
+
+    @Column(name = "line_flow_color_mode")
+    private String lineFlowColorMode = "nominalVoltage";
+
+    @Column(name = "line_flow_alert_threshold")
+    private Integer lineFlowAlertThreshold = 100;
+
+    @Column(name = "map_manual_refresh")
+    private Boolean mapManualRefresh = false;
+
+    @Column(name = "map_basemap")
+    private String mapBaseMap = "mapbox";
+
+    @Column(name = "diagonal_label")
+    private Boolean diagonalLabel = false;
+
+    @Column(name = "center_label")
+    private Boolean centerLabel = false;
+
+    @Column(name = "substation_layout")
+    private String substationLayout = "horizontal";
+
+    @Column(name = "component_library")
+    private String componentLibrary = "";
+
+    @Column(name = "init_nad_with_geo_data")
+    private Boolean initNadWithGeoData = true;
+}
