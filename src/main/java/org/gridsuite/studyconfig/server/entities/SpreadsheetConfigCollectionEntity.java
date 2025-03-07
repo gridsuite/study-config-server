7839ab6cbea0f36949693799c0cfadbe122a6eb7
achour94 <berrahmaachour@gmail.com>
2025-03-07 16:21:51 +0100

    Add reorder spreadsheet configs
    Signed-off-by: achour94 <berrahmaachour@gmail.com>

--- src/main/java/org/gridsuite/studyconfig/server/entities/SpreadsheetConfigCollectionEntity.java
+++ src/main/java/org/gridsuite/studyconfig/server/entities/SpreadsheetConfigCollectionEntity.java
@@ -32,6 +32,7 @@ public class SpreadsheetConfigCollectionEntity {
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "collection_id", foreignKey = @ForeignKey(name = "fk_spreadsheet_config_collection"))
+    @OrderColumn(name = "config_order")
     @Builder.Default
     private List<SpreadsheetConfigEntity> spreadsheetConfigs = new ArrayList<>();
 }
