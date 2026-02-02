a1f913630a641669fa7ee9aa3342fbce455458d8
Rehili Ghazwa <ghazwarhili@gmail.com>
2026-02-02 13:02:40 +0100

    fix sql script

--- src/main/resources/db/changelog/changesets/migrationColumnFilter_20260201T111008Z.sql
+++ src/main/resources/db/changelog/changesets/migrationColumnFilter_20260201T111008Z.sql
@@ -1,22 +1,36 @@
-INSERT INTO column_filter (uuid, filter_data_type, filter_type, filter_value, filter_tolerance)
-SELECT
-    gen_random_uuid(),
-    filter_data_type,
-    filter_type,
-    filter_value,
-    filter_tolerance
-FROM spreadsheet_column
-WHERE
-    filter_data_type IS NOT NULL
-   OR filter_type IS NOT NULL
-   OR filter_value IS NOT NULL
-   OR filter_tolerance IS NOT NULL;
-
+WITH created_filters AS (
+    SELECT
+        sc.uuid AS column_uuid,
+        gen_random_uuid() AS filter_uuid,
+        sc.filter_data_type,
+        sc.filter_type,
+        sc.filter_value,
+        sc.filter_tolerance
+    FROM spreadsheet_column sc
+    WHERE
+        sc.filter_data_type IS NOT NULL
+       OR sc.filter_type IS NOT NULL
+       OR sc.filter_value IS NOT NULL
+       OR sc.filter_tolerance IS NOT NULL
+),
+     inserted AS (
+         INSERT INTO column_filter (
+                                    uuid,
+                                    filter_data_type,
+                                    filter_type,
+                                    filter_value,
+                                    filter_tolerance
+             )
+             SELECT
+                 filter_uuid,
+                 filter_data_type,
+                 filter_type,
+                 filter_value,
+                 filter_tolerance
+             FROM created_filters
+             RETURNING uuid
+     )
 UPDATE spreadsheet_column sc
-SET column_entity_column_filter_id = cf.uuid
-FROM column_filter cf
-WHERE
-    sc.filter_data_type IS NOT DISTINCT FROM cf.filter_data_type
-  AND sc.filter_type IS NOT DISTINCT FROM cf.filter_type
-  AND sc.filter_value IS NOT DISTINCT FROM cf.filter_value
-  AND sc.filter_tolerance IS NOT DISTINCT FROM cf.filter_tolerance;
+SET column_entity_column_filter_id = cf.filter_uuid
+FROM created_filters cf
+WHERE sc.uuid = cf.column_uuid;
